00956bcaf0955e32378b00e7716ee4e055172ace
SALutHere <leetmansup@gmail.com>
2025-06-18 18:42:18 +0300

    Featured listing of expenses

---
+++ cmd/list.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"expense-tracker/internal/expenses"
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all the expenses",
+	Run: func(cmd *cobra.Command, args []string) {
+		exps, err := expenses.GetExpensesFromFile()
+		if err != nil {
+			log.Fatalf("error when listing expenses: %v", err)
+		}
+		output := expenses.GetExpensesTable(exps)
+		fmt.Println(output)
+	},
+}
---
+++ internal/expenses/service.go
@@ -0,0 +1,29 @@
+package expenses
+
+import (
+	"bytes"
+	"fmt"
+	"text/tabwriter"
+
+	"github.com/google/uuid"
+)
+
+// Returns table format of given expenses
+func GetExpensesTable(expenses map[uuid.UUID]Expense) string {
+	var buffer bytes.Buffer
+	var writer = tabwriter.NewWriter(&buffer, 0, 8, 1, '\t', 0)
+
+	fmt.Fprintln(writer, "ID\tDescription\tAmount\tCategory\tDate")
+	for _, expense := range expenses {
+		fmt.Fprintf(writer, "%s\t%s\t%.2f\t%s\t%s\n",
+			expense.ID.String(),
+			expense.Description,
+			expense.Amount,
+			expense.Category,
+			expense.Date.Format("2006-01-02 15:04:05"),
+		)
+	}
+	writer.Flush()
+
+	return buffer.String()
+}
